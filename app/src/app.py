47859db00b4ef0034868e6b9b547e5e21f135966
Narapady <chhuoynarapady@yahoo.com>
2024-04-17 16:40:56 -0700

    second commit

--- app/src/app.py
+++ app/src/app.py
@@ -3,10 +3,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    return "Hello world everyone"
+    return "Hello world everyone!"
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host="0.0.0.0")
